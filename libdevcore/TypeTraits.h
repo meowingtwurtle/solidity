f63124399e41a3af15cd028ee9db19a3d8b57d00
Jason Cobb <jason.e.cobb@gmail.com>
2019-12-16 22:15:49 -0500

    Add utility type trait enable_if_same_up_to_const_t

---
+++ libdevcore/TypeTraits.h
@@ -0,0 +1,28 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <type_traits>
+
+namespace dev
+{
+
+template <class First, class Second>
+using enable_if_same_up_to_const_t = std::enable_if_t<std::is_same_v<std::remove_const_t<First>, std::remove_const_t<Second>>>;
+
+}
